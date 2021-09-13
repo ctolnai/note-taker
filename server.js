a1722a190a77d1cd7b61391184e620bd97c0d632
Chris Tolnai <christolnai@gmail.com>
2021-09-13 14:56:14 -0700

    added middleware

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+const PORT = process.env.port || 3001;
+const app = express();
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
