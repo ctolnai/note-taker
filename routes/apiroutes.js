046ce29cf96b674499a21c69542ca4f8c48bf34c
Chris Tolnai <christolnai@gmail.com>
2021-09-14 14:38:57 -0700

    updated port

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -6,10 +6,11 @@ const {readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
 
 router.get('/notes', (req, res) => {
-    readFromFile('./db/db.json').then ((data)=>res.json(JSON.parse(data)));
+    console.info(`${req.method} request received for feedback`);
+    
+    readFromFile('./db/db.json').then ((data) => res.json(JSON.parse(data)));
 
-}
-);
+});
 
 router.post('/notes', (req, res) => {
     console.info(`${req.method} request received to submit feedback`);
