c2e371d6ba774367633368baaaa67d42c6bc3d11
Chris Tolnai <christolnai@gmail.com>
2021-09-15 10:52:23 -0700

    added delete function

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -3,7 +3,7 @@ const { ifError } = require('assert');
 const { randomUUID } = require('crypto');
 const fs = require('fs');
 const db = require('../db/db.json');
-const {readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const {readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils');
 
 
 router.get('/notes', (req, res) => {
@@ -24,7 +24,7 @@ router.post('/notes', (req, res) => {
             text,
             id: randomUUID() 
         };
-        // console.log (newNote);
+        
 
         readAndAppend(newNote, 'db/db.json');
 
@@ -37,10 +37,25 @@ router.post('/notes', (req, res) => {
       } else {
         res.json('Error in posting feedback');
       }
-    
+
 }
 );
 
 
+
+router.delete('/notes/:id', (req, res) => {
+    console.info(`${req.method}`)
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+        const newNoteArray = json.filter(note => note.id !== noteId)
+        writeToFile('./db/db.json', newNoteArray)
+        res.status(200).json('Note Deleted');
+        return;
+    })
+
+
+});
 
 module.exports = router
